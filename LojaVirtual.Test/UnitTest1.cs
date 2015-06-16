d251bd03df31abfc8808790edada1f3e48e913d8
Gilson <gilsonlig100@gmail.com>
2015-06-16 15:35:30 -0300

    teste

--- LojaVirtual.Test/UnitTest1.cs
+++ LojaVirtual.Test/UnitTest1.cs
@@ -1,6 +1,10 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿
 
+
+using LojaVirtual.Web.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Web.Mvc;
 namespace LojaVirtual.Test
 {
     [TestClass]
@@ -9,6 +13,24 @@ namespace LojaVirtual.Test
         [TestMethod]
         public void TestMethod1()
         {
+        }
+
+         [TestMethod]
+        public void TestarSePaginacaoEstaGerandoCorretamente()
+        {
+             HtmlHelper html = null;
+             Paginacao paginacao = new Paginacao
+             {
+                 PaginaAtual = 2,
+                 ItensPorPagina = 10,
+                 ItensTotal = 28
+
+             };
+
+             Func<int, string> paginaUrl = i => "pagina" + i;
+             //MvcHtmlString resultado = html.PageLinks(paginacao, paginaUrl);
+
+             
         }
     }
 }
---
+++ LojaVirtual.Web/Models/ProdutosViewModel.cs
@@ -0,0 +1,15 @@
+﻿using LojaVirtual.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Web.Models
+{
+    public class ProdutosViewModel
+    {
+        public IEnumerable<Produto> Produtos { get; set; }
+        public Paginacao Paginacao { get; set; }
+    }
+}
